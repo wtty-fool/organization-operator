8cf36552d292e3b5f25c01cedbc0cc84dd021cd0
wtty-fool <wtty.fool@gmail.com>
2020-05-28 12:23:49 +0200

    go: remove bundleVersion

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,17 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.0.1"
-	description   = "The organization-operator does something."
-	gitSHA        = "n/a"
-	name          = "organization-operator"
-	source        = "https://github.com/giantswarm/organization-operator"
-	version       = "1.0.0-dev"
+	description = "The organization-operator does something."
+	gitSHA      = "n/a"
+	name        = "organization-operator"
+	source      = "https://github.com/giantswarm/organization-operator"
+	version     = "0.1.0-dev"
 )
-
-func BundleVersion() string {
-	return bundleVersion
-}
 
 func Description() string {
 	return description
