e5ef502087b704d6202e65e7c6dd75d50f5ce9f4
wtty-fool <wtty.fool@gmail.com>
2020-06-03 10:47:41 +0200

    go: erase reconciliation loop

--- service/controller/resource/organization/delete.go
+++ service/controller/resource/organization/delete.go
@@ -2,30 +2,8 @@ package organization
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/giantswarm/microerror"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-
-	"github.com/giantswarm/organization-operator/service/controller/key"
 )
 
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	org, err := key.ToOrganization(obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	orgNamespace := newOrganizationNamespace(org.ObjectMeta.Name)
-
-	err = r.k8sClient.CtrlClient().Delete(context.Background(), orgNamespace)
-	if apierrors.IsNotFound(err) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("organization namespace %#q does not exist", org.ObjectMeta.Name))
-		return nil
-	} else if err != nil {
-		return microerror.Mask(err)
-	}
-
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("organization namespace %#q has been deleted", org.ObjectMeta.Name))
 	return nil
 }
