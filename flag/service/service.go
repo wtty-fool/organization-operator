7efb144e9f41bdc00e19d35cb5226d28dad98e78
Adrian Barsan <mnlk123@yahoo.com>
2021-05-17 11:34:21 +0200

    Add flag for configuring the legacy organizations service

--- flag/service/service.go
+++ flag/service/service.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"github.com/giantswarm/operatorkit/v2/pkg/flag/service/kubernetes"
+	"github.com/giantswarm/organization-operator/flag/service/legacyorganizations"
 )
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
-	Kubernetes kubernetes.Kubernetes
+	Kubernetes          kubernetes.Kubernetes
+	LegacyOrganizations legacyorganizations.LegacyOrganizations
 }
