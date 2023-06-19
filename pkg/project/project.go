ed0860e83435170d3c239c925bc26c66cca13732
taylorbot <dev@giantswarm.io>
2023-06-19 10:27:29 +0000

    Bump version to 1.2.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "organization-operator"
 	source      = "https://github.com/giantswarm/organization-operator"
-	version     = "1.2.0"
+	version     = "1.2.1-dev"
 )
 
 func Description() string {
