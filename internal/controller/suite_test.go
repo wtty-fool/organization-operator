14ba45b77fed504a0a010dd056903c9a33059c9c
Spyros Synodinos <spyros@giantswarm.io>
2024-07-17 13:38:39 +0300

    useExistingCluster := false

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -1,17 +1,3 @@
-/*
-Copyright 2024.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package controller
 
 import (
@@ -47,11 +33,7 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 
-	useExistingCluster := true
-	if os.Getenv("USE_EXISTING_CLUSTER") == "false" {
-		useExistingCluster = false
-	}
-
+	useExistingCluster := false
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
